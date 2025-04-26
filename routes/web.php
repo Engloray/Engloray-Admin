0448fc641b5e18bdc401349c4d7b9a103bade36f
surya <sksurya6074@gmail.com>
2025-04-26 18:03:52 +0530

    admin Url Update

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 Route::get('/admin/login', [AdminController::class, 'showLoginForm'])->name('admin.login');
-Route::post('/admin/login', [AdminController::class, 'login']);
-Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+Route::post('/admin/login', [AdminController::class, 'login'])->middleware('throttle:5,1');
 Route::get('/admin/logout', [AdminController::class, 'logout'])->name('admin.logout');
 Route::get('/admin/search-contacts', [AdminController::class, 'searchContacts'])->name('admin.searchContacts');
+Route::get('/', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 Route::post('/contact', [ContactFormController::class, 'store'])->middleware('throttle:10,1')->name('contact.store');
